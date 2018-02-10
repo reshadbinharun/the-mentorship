409987fce91bdd63e0629469a8e48f12f98251dd
Md Reshad Bin Harun <reshadbinharun@Mds-MacBook-Pro.local>
2018-02-10 01:45:21 -0500

    index try 2

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
