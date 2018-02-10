08df6a55a7077ab8e88f4060bba3b3f14cb70d6b
Md Reshad Bin Harun <reshadbinharun@Mds-MacBook-Pro.local>
2018-02-10 01:38:21 -0500

    index try 2

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-app.use("/styles", express.static(__dirname));
-app.use("/images", express.static(__dirname + '/images'));
-app.use("/scripts", express.static(__dirname + '/scripts'));
+//app.use("/styles", express.static(__dirname));
+//app.use("/images", express.static(__dirname + '/images'));
+//app.use("/scripts", express.static(__dirname + '/scripts'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
 });
 
 // add other routes below
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname + 'views/about.html'));
-});
+//app.get('/about', function (req, res) {
+//  res.sendFile(path.join(__dirname + 'views/about.html'));
+//});
 
 app.listen(process.env.PORT || 8080);
