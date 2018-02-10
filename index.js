f6ed7a43de5a439500fa6f08b7dfc6dff62a4ede
Md Reshad Bin Harun <reshadbinharun@Mds-MacBook-Pro.local>
2018-02-10 04:08:55 -0500

    db

--- index.js
+++ index.js
@@ -29,9 +29,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.post('/submit', function (req,res){
-	res.sendFile(path.join(__dirname + '/public/mentors.html'));
-})
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 
 app.listen(process.env.PORT || 3000);
 
